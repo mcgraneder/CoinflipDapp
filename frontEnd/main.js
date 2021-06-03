fc0eb5f47765284ebef4b2185eb54b0c6ff59862
Evan McGrane <evan.mcgrane5@mail.dcu.ie>
2021-06-03 01:46:57 +0100

    fixed bugs Stilll need to do Oracle Randomness

--- frontEnd/main.js
+++ frontEnd/main.js
@@ -1,6 +1,8 @@
 var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
-contract_address = "0xFB327A84FbE2019AF3b83919681E65eB8fa0a589";
+contract_address = "0x930ca5F7029D8Cb5bA1a0a49A668Ae7C0917a21F";
+var Stop;
+var exit;
 
 $(document).ready(function() {
   
@@ -20,13 +22,30 @@ $(document).ready(function() {
     $("#add_bet_button").click(betData)
 
     $("#flip_coin_button").click(flipData)
+
+    $("#withdraw_coin_button").click(withdrawData)
     //insyanciate the get data button
    
 
 })
 
 function betData(){
+    
+    
     //to input data we need the values of the create person forum
+    // contractInstance.methods.getBetStatus().call().then(function(res){
+    //     if (res == true) {
+    //         console.log("I MADE IT");
+    //         setTimeout(function () {
+    //             document.getElementById("popup-input").classList.toggle("active");
+    //         }, 40000)
+            
+    //         if (i != 0) { return; }
+    //     }
+    // }) 
+
+
+    
     var betAmount = $("#bet_input").val(); //.val() gets the value 
    
     var config = {
@@ -64,27 +83,87 @@ function betData(){
 }
 
 async function flipData(){
-    //to input data we need the values of the create person forum
-    // var betAmount = $("#bet_input").val(); //.val() gets the value 
-   
-    // var config = {
-    //     value: web3.utils.toWei(String(betAmount), "ether")
-    // }
     
-    const randomSeed = await contractInstance.methods.flipCoin().call().then( async function(res) {
-        $("#bet_output").text("You won");
-    });
-    console.log(randomSeed);
+    var winAmount = $("#bet_input").val() * 2;
+    var looseAmount = $("#bet_input").val();
 
-    const owner = contractInstance.methods.getPlayer().call();
+    let output = contractInstance.methods.flipCoin().call().then(function (res) {
+        console.log(res);
+        if(res == true) {
+            console.log("goodbye")
+        }
+        else if (res == false) {
+            console.log("hellow");
+            // return(res.data);
+        }
+        contractInstance.methods.settleBet(res).send().then(function (out) {
 
-    owner.then(function(res) {
-        $("#address_output").text(String(res));
+
+            if(res == true) {
+                checkForLoad();
+                setTimeout(function () {
+                    $("#win-loose").text("Congratulations. You won!");
+                }, 3500)
+
+                setTimeout(function () {
+                    $("#win-loose-prize").text("Winnings:\n" + String(winAmount) + " Eth");
+                }, 4500)
+
+                setTimeout(function () {
+                    const balance = contractInstance.methods.getPlayerBalance().call().then(function(balance) {
+                        $("#win-loose-balance").text("Balance:\n" + String(balance) + " Eth");
+                    });
+                   
+                }, 5000)
+                $("#bet-output").text("You won " + String(balance) + " Eth");
+            }
+            else {
+
+                checkForLoad();
+                setTimeout(function () {
+                    $("#win-loose").text("Oops you lost. Hard luck!");
+                }, 3500)
+
+                setTimeout(function () {
+                    $("#win-loose-prize").text("Loosings:\n" + String(looseAmount) + " Eth");
+                }, 4500)
+
+                setTimeout(function () {
+                    const balance = contractInstance.methods.getPlayerBalance().call().then(function(balance) {
+                        $("#win-loose-balance").text("Balance:\n" + String(balance) + " Eth");
+                    });
+                   
+                }, 5000)
+                $("#bet-output").text("You won " + String(balance) + " Eth");
+                
+                
+            }
+        })
+        
+       
     })
+ 
+    const balance = contractInstance.methods.getPlayerBalance().call().then(function(balance) {
+        $("#bet-output").text(String(balance) + " Eth");
+    })
+    console.log(balance);
+    
+   
+   
+}
+
+function withdrawData() {
 
     
+    var config = {
+        value: web3.utils.toWei("3", "ether")
+    }
+    const balance = contractInstance.methods.getPlayerBalance().call().then(function(balance) {
+        $("#bet-output").text(String(balance) + " Eth");
+    })
+    console.log(balance);
+    contractInstance.methods.withdraw().send().then();
 
-   
 }
 
 function togglePopup() {
@@ -109,6 +188,13 @@ function checkForBlank() {
         document.getElementById("popup-flip").classList.toggle("active");
 
     }
+    // contractInstance.methods.getBetStatus().call().then(function(res) {
+    //     if(res == true) {
+    //         Stop = true;
+    //         document.getElementById("popup-input").classList.toggle("active");
+    //     }
+    // })
+   
 }
 
 
@@ -120,5 +206,27 @@ function checkForBlankFlip() {
     
         }
     });
+    
+}
+
+function checkForBlankWithdraw() {
+    //handle in future by saying if isActive = flase then cannot flip coin
+    const balance = contractInstance.methods.getPlayerBalance().call().then( function(res) {
+        if (res == 0) {
+            document.getElementById("popup-withdraw").classList.toggle("active");
+    
+        }
+    });
+    
+}
+
+function checkForLoad() {
+    //handle in future by saying if isActive = flase then cannot flip coin
+    
+    document.getElementById("popup-load").classList.toggle("active");
+    // document.getElementById("Winnings").classList.toggle("a")
+    
+        
+    
     
 }
